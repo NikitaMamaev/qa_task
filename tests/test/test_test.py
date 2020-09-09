acd799e9d34b8fd7fcec1de56344871563744f6c
NikitaMamaev <lazy.raccoon@yandex.ru>
2020-09-09 13:53:53 +0800

    api requests works!

--- tests/test/test_test.py
+++ tests/test/test_test.py
@@ -1,8 +1,19 @@
-
-
 from utils.api_requests import send_request
 
 
 def test_test():
+
+    payload = {
+        'tmpl_id': 'autotest_id_1'
+    }
+
+    files = open('./temp_file.yaml', 'rb')
+
+    send_request(
+        method='put',
+        data=payload,
+        files=files
+    )
+
     response = send_request()
     print(f"\nResponse: {response}\n")
--- utils/api_requests.py
+++ utils/api_requests.py
@@ -18,13 +18,15 @@ urllib3.disable_warnings()
 def send_request(
     method: str = 'get',
     url: str = f"{settings.URL}{settings.API_HANDLER}",
-    data: Optional[dict] = None
+    data: Optional[dict] = None,
+    files = None,
 ) -> str:
     """
     Send API-request to service
     :param method: request method: GET, POST, PUT or DELETE
     :param url: request url
     :param data: POST- or PUT-request data
+    :param files: POST- or PUT-request files
     """
 
     method = method.lower()
@@ -34,12 +36,16 @@ def send_request(
 
     response = getattr(requests, method)(
         url=url,
-        data=json.dumps(data)
+        data={'data': json.dumps(data)} if data else None,
+        files={'file': files} if files else None
     )
 
     hc.assert_that(
         actual=response.status_code,
-        matcher=hc.equal_to(200),
+        matcher=hc.any_of(
+            hc.equal_to(200),
+            hc.equal_to(201)
+        ),
         reason=f"response = {response.content}; status_code = {response.status_code}"
     )
 
